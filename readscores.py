985adc264fca374a30a03e437d475d10929932d1
Murat DURNA <mratdrn@gmail.com>
2025-08-06 16:14:31 +0300

    fix: read scores from redis

---
+++ readscores.py
@@ -0,0 +1,21 @@
+import redis
+import json
+
+# Redis bağlantısı
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+def read_scores():
+    data = r.get("live_scores")
+    if data:
+        try:
+            parsed = json.loads(data)
+
+            # JSON kodunun okunaklığı için;
+            print(json.dumps(parsed, indent=2, ensure_ascii=False))
+        except Exception as e:
+            print("JSON parse hatası:", e)
+    else:
+        print("Redis'te canlı skor verisi bulunamadı.")
+
+if __name__ == "__main__":
+    read_scores()
